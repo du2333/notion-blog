057a46c7f1322f77f64063f774bfb86fe5673057
Zephyrrr <kuangdadu@gmail.com>
2025-03-22 21:24:43 +0000

    Update blog configuration with environment variables and example content

--- blog.config.ts
+++ blog.config.ts
@@ -1,17 +1,17 @@
 import { blogConfig } from "@/types/config";
 
 export default blogConfig({
-  NOTION_PAGE_ID: "1be1f833110780ba87acfa2cf9a6a8c4",
+  NOTION_PAGE_ID: process.env.NOTION_PAGE_ID || "1be1f833110780d98383fc637676cee8",
   NOTION_HOST: "https://www.notion.so",
   NEXT_REVALIDATE_SECONDS: 60,
 
-  AUTHOR: "KD",
-  BLOG_TITLE: "Blog",
-  BLOG_DESCRIPTION: "Blog Description",
+  AUTHOR: "Example",
+  BLOG_TITLE: "Example Blog",
+  BLOG_DESCRIPTION: "Example Blog Description",
   BLOG_HOME_BANNER: "/public/banner.png",
   BLOG_ICON: "/public/avatar.png",
-  EMAIL: "kuangdadu@gmail.com",
-  GITHUB: "https://github.com/du2333",
+  EMAIL: "example@gmail.com",
+  GITHUB: "https://github.com/example",
 
   POSTS_PER_PAGE: 10,
   POST_LIST_STYLE: "page",
