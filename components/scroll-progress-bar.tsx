e33a135b6dd201e5a3f7ef1caacfd0b3824d3102
Zephyrrr <kuangdadu@gmail.com>
2025-04-09 02:31:35 +0000

    Enhance ScrollProgressBar component to show/hide based on scroll position;

--- components/scroll-progress-bar.tsx
+++ components/scroll-progress-bar.tsx
@@ -1,15 +1,30 @@
 "use client";
 
 import { ScrollProgress } from "@/components/ui/scroll-progress";
+import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function ScrollProgressBar() {
   const pathname = usePathname();
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsVisible(window.scrollY > 100);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   // only show in article page
   if (!pathname.includes("/post")) {
     return null;
   }
 
-  return <ScrollProgress className="top-16" />;
+  return (
+    <ScrollProgress
+      className={cn("top-16 opacity-0", isVisible && "opacity-100")}
+    />
+  );
 }
